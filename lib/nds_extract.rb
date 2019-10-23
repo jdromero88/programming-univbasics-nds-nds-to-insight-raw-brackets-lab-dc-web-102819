c15d40771070623a84b0c183b4e048c3795142f5
José Romero <jdromerov88@gmail.com>
2019-10-23 06:47:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,8 +18,7 @@ def directors_totals(nds)
   print sum
   i = 0
   while i < directors_database.count do
-    name[i] = directors_database[i]
-    print name[i]
+    puts directors_database[i]
     i += 1
   end
   #grand_total = 0
