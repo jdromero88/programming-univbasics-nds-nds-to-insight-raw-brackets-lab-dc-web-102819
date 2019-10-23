8ce01cc585e588f2d73fa96cdd3528ca226e207c
José Romero <jdromerov88@gmail.com>
2019-10-23 05:09:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,7 +8,7 @@ def directors_totals(nds)
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   #pp nds
-  puts directors_database[:name][:movies]
+  puts directors_database[0][:name][:movies]
   grand_total = 0
   result = {
   }
